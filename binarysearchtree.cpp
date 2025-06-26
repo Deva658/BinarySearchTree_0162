e8ea890b82ae133b02586378b67f124f8735285b
Deva658 <devaaditya271@gmail.com>
2025-06-26 10:00:40 +0700

    menambahkan konstruktor untuk kelas Node

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -8,5 +8,10 @@ public:
     Node *leftchild;
     Node *rightchild;
     
-    //
-}
+    //constructor for the Node class
+    Node()
+    {
+        leftchild = nullptr; // Initialize left child to null
+        rightchild = nullptr; // Initialize right child to null
+    }
+};
