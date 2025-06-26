3f2f8d472083f2225ff92183ce481c08d94f26e5
Deva658 <devaaditya271@gmail.com>
2025-06-26 10:40:30 +0700

    menambahkan logika untuk menangani kasus ketika pohon kosong dalam metode insert kelas BinaryTree

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -47,5 +47,15 @@ public:
         Node *currentNode = nullptr;
         search(x, parent, currentNode);
 
+        //step 5: if parent is NULL (tree is empty)
+        if (parent == nullptr) 
+        {
+            //5a: mark the new node as ROOT
+            ROOT = newNode;
+
+            //5B: EXIT
+            return;
+        } 
+
 
 };
